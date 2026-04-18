d39822331a655bfdc5cd9cf542de9c0c575bc95f
Tushar Ramgirkar <tusharramgirkar99@gmail.com>
2026-04-18 11:36:51 +0530

    restructure project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,5 @@ const Home = () => {
     <div className="min-h-dvh flex items-center justify-center">Home</div>
   )
 }
-export const metadata = {
-  title: 'Home',
-  description: 'Hi There abc!',
-  date: '20/06'
-}
+
 export default Home
